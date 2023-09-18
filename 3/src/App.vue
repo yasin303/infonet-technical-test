f9121ddc160e04e47db459e364c899e7acdb0861
yasin303 <myasinabdulhafiidh@gmail.com>
2023-09-18 08:09:37 +0700

    feat (number 3):add feature button add item, form input and delete

--- 3/src/App.vue
+++ 3/src/App.vue
@@ -1,47 +1,73 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+	<main>
+		<h1>Technical Test Number 3</h1>
+		<!-- button -->
+		<div>
+			<button @click="addItem">+ Add Item</button>
+		</div>
+		<form @submit.prevent="input">
+			<div v-for="input in form" :key="input.key">
+				<select v-model="input.criteria" name="criteria" placeholder="Criteria">
+					<!-- form criteria -->
+					<option value="">Criteria</option>
+					<option v-for="opt in selection" :key="opt" :value="opt">
+						{{ opt }}
+					</option>
+				</select>
+				<!-- form input data -->
+				<input v-model="input.input" type="text" placeholder="Input" />
+				<button @click="delete" @submit.prevent>X</button>
+			</div>
+		</form>
+		<!-- show form -->
+		<code>
+			<pre>{{ showForm }}</pre>
+		</code>
+	</main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<script>
+export default {
+	data() {
+		return {
+			selection: [
+				"Age",
+				"Education",
+				"Interest",
+				"Language",
+				"Marital Status",
+				"Skill",
+			],
+			form: [
+				{
+					criteria: "",
+					input: "",
+				},
+			],
+		};
+	},
+	methods: {
+		addItem: function () {
+			this.form.push({
+				criteria: "",
+				input: "",
+			});
+		},
+		delete: function (event) {
+			this.form.splice(this.form.indexOf(event), 1);
+		},
+		input: function () {
+			console.log(this.form);
+		},
+	},
+	computed: {
+		showForm() {
+			const dataForm = this.form.map((data) => ({
+				criteria: data.criteria,
+				input: data.input,
+			}));
+			return JSON.stringify(dataForm, undefined, 2);
+		},
+	},
+};
+</script>
